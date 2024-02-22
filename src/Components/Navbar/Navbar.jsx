aaf2ad8645b279dab2a2a9412d8972cf0bec8fdb
Duygu Yagmur <30656517+DYagmur@users.noreply.github.com>
2024-02-22 13:09:55 -0800

    Footer and Assets

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,76 +1,84 @@
 import React, {useState} from 'react'
-import './navbar.css'
-import {MdOutlineTravelExplore} from  "react-icons/md";
-import {AiFillCloseCircle} from  "react-icons/ai";
-import {TbGridDots} from  "react-icons/tb";
+import "./navbar.css"
 
-const Navbar = () => {
-    const [active, setActive] = useState('navBar')
+import {MdTravelExplore} from 'react-icons/md'
+import {AiFillCloseCircle} from 'react-icons/ai'
+import {TbGridDots} from 'react-icons/tb'
+
+export const Navbar = () => {
+
+    const [active, setActive] = useState('navBar') ;
+
     // function to toggle navBar
-    const showNav = ()=>{
-      setActive('navBar activeNavbar')
+    const showNav = ()=> {
+        setActive('navBar activeNavbar');
     }
 
+    // function to remove navBar
+    const removeNavbar = ()=> {
+        setActive('navBar');
+    }
+
+
   return (
-    <section className='navBarSection'>
-      <header className="header flex">
-        <div className="logoDiv">
-          <a href="#" className="logo">
-            <h1><MdOutlineTravelExplore className="icon"/> Travel.</h1>
-          </a>
-        </div>
+    <section className='navBarSection' >
+        <header className='header flex'>
+            <div className='logoDiv'>
+                <a href='#' className='logo flex'>
+                    <h1> <MdTravelExplore className='icon' /> Travel</h1>
+                </a>
+            </div>
 
-        <div className={active}>
-          <ul className="navLists flex">
-            <li className="navItem">
-              <a href="#" className="navLink">Home</a>
-            </li>
+            <div className={active}>
+                <ul className='navList flex'>
 
-            <li className="navItem">
-              <a href="#" className="navLink">Packages</a>
-            </li>
+                    <li className='navItem'>
+                        <a href='#' className='navLink'>Home</a>
+                    </li>
 
-            <li className="navItem">
-              <a href="#" className="navLink">Shop</a>
-            </li>
+                    <li className='navItem'>
+                        <a href='#' className='navLink'>Packages</a>
+                    </li>
 
-            <li className="navItem">
-              <a href="#" className="navLink">About</a>
-            </li>
+                    <li className='navItem'>
+                        <a href='#' className='navLink'>Shop</a>
+                    </li>
 
-            <li className="navItem">
-              <a href="#" className="navLink">Pages</a>
-            </li>
+                    <li className='navItem'>
+                        <a href='#' className='navLink'>About</a>
+                    </li>
 
-            <li className="navItem">
-              <a href="#" className="navLink">News</a>
-            </li>
+                    <li className='navItem'>
+                        <a href='#' className='navLink'>Pages</a>
+                    </li>
 
-            <li className="navItem">
-              <a href="#" className="navLink">Contact</a>
-            </li>
+                    <li className='navItem'>
+                        <a href='#' className='navLink'>News</a>
+                    </li>
 
-            <button className='btn'>
-              <a href="#">BOOK NOW</a>
-            </button>
-            
-          </ul>
+                    <li className='navItem'>
+                        <a href='#' className='navLink'>Contact</a>
+                    </li>
 
-          <div className="closeNavbar">
-          <AiFillCloseCircle className="icon"/>
+                    <button className='btn'>
+                        <a href="#">BOOK NOW</a>
+                    </button>
 
-          </div>
+                </ul>
 
-        </div>
+                <div onClick={removeNavbar} className="closeNavbar">
+                    <AiFillCloseCircle className='icon'/>
+                </div>
 
-        <div onClick={showNav} className="toggleNavbar">
-        <TbGridDots className="icon"/>
-        </div>
+            </div>
 
-      </header>
-      
+            <div onClick={showNav} className="toggleNavbar">
+                    < TbGridDots className='icon' />
+            </div>
+
+        </header>
     </section>
   )
 }
 
-export default Navbar
+export default Navbar;
